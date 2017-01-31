042ff900696ceacbcdc44e3c17e4a32385ad0398
Abel Zhang <zjplab@gmail.com>
2017-01-31 12:00:23 +0800

    Create Newton's Method.scala

---
+++ Functional Program Principle in Scala/Functions and Evaluation/Newton's Method.scala
@@ -0,0 +1,13 @@
+object session{
+def abs(x:Double) = if (x < 0) -x else x
+
+def sqrtIter(guess: Double, x: Double): Double =
+  if (isGoodEnough(guess, x)) guess
+  else sqrtIter(improve(guess, x), x)
+
+def isGoodEnough(guess: Double, x: Double) = abs(guess-x)/x < 1e-3
+
+def improve(guess: Double, x: Double) =(guess + x / guess) / 2
+
+def sqrt(x: Double) = sqrtIter(1.0, x)
+}
