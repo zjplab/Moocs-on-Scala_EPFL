b4ef12fd97869752e74177a4c20f9804ebe1833d
JP Zhang <zjplab@users.noreply.github.com>
2018-02-27 21:47:51 +0800

    Update Newton's Method.scala

--- Functional Program Principle in Scala/Functions and Evaluation/Newton's Method.scala
+++ Functional Program Principle in Scala/Functions and Evaluation/Newton's Method.scala
@@ -5,7 +5,7 @@ def sqrtIter(guess: Double, x: Double): Double =
   if (isGoodEnough(guess, x)) guess
   else sqrtIter(improve(guess, x), x)
 
-def isGoodEnough(guess: Double, x: Double) = abs(guess-x)/x < 1e-3
+def isGoodEnough(guess: Double, x: Double) = abs(guess*guess -x)/x < 1e-3
 
 def improve(guess: Double, x: Double) =(guess + x / guess) / 2
 
