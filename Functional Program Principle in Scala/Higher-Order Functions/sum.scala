24a9e1e2eb455a140601cbbafc6a2e4e44f2cf3c
JP Zhang <zjplab@users.noreply.github.com>
2018-02-28 17:48:20 +0800

    Create sum.scala

---
+++ Functional Program Principle in Scala/Higher-Order Functions/sum.scala
@@ -0,0 +1,11 @@
+def sum(f:Int->Int, a:Int, b:Int):Int=
+  if(a>b) 0
+  else f(a)+sum(f,a+1.b)
+  
+def sumInt(a:Int,b:Int):Int=sum(id,a,b)
+def sumCubes(a:Int,b:Int):Int=sum(cube,a,b)
+def sumFactorials(a:Int,b:Int):Int=sum(fact,a,b)
+
+def id(x:Int)=x
+def cube(x:Int)=x*x
+def fact(x:Int):Int=if(x==0) 1 else x*fact(x-1)
