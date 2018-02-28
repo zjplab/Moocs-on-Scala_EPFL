463a2c576f30b83ca6e4508ef00e2982a00ff294
JP Zhang <zjplab@users.noreply.github.com>
2018-02-28 18:17:24 +0800

    Update sum.scala

--- Functional Program Principle in Scala/Higher-Order Functions/sum.scala
+++ Functional Program Principle in Scala/Higher-Order Functions/sum.scala
@@ -3,9 +3,22 @@ def sum(f:Int=>Int, a:Int, b:Int):Int=
   else f(a)+sum(f,a+1.b)
   
 def sumInt(a:Int,b:Int):Int=sum(id,a,b)
-def sumCubes(a:Int,b:Int):Int=sum(cube,a,b)
-def sumFactorials(a:Int,b:Int):Int=sum(fact,a,b)
+def sumCube(a:Int,b:Int):Int=sum(cube,a,b)
+def sumFactorial(a:Int,b:Int):Int=sum(fact,a,b)
 
 def id(x:Int)=x
 def cube(x:Int)=x*x
 def fact(x:Int):Int=if(x==0) 1 else x*fact(x-1)
+
+//Summation with Anonymous Functions 
+def sumInt(a:Int,b:Int):Int=sum(x=>x,a,b)
+def sumCube(a:Int,b:Int):Int=sum(x=>x*x,a,b)
+def sumFactorial(a:Int,b:Int):Int=sum(fact,a,b)
+
+//tail-recursive sum
+def sum(f:Int=>Int, a:Int, b:Int):Int={
+  def loop(a:Int,acc:Int)=
+    if(a>b) acc
+    else loop(a+1,acc+f(a))
+  loop(a,0)
+}
