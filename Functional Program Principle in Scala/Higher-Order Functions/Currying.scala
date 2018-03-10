760a58b7f52182cb4bb032121a7c4c915e07ec24
JP Zhang <zjplab@users.noreply.github.com>
2018-03-10 23:06:21 +0800

    Update Currying.scala

--- Functional Program Principle in Scala/Higher-Order Functions/Currying.scala
+++ Functional Program Principle in Scala/Higher-Order Functions/Currying.scala
@@ -14,7 +14,11 @@ def sumFactorials=sum(fact)
 def product(f:Int=>Int)(a:Int,b:Int):Int=
     if(a>b) 1 else f(a)*product(f)(a,b)
 
-//generalization
+def fact(n:Int)=product(x=>x)(1,n)
+
+//generalization with algebra flavour, addition and multiplication are nothing but binary operation under a group, which has zero and unit
+// elements 
 def mapReduce(f:Int=>Int, combine:(Int,Int)=>Int,zero:Int)(a:Int,b:Int):Int=
     if(a>b) zero
     else combine(f(a),mapReduce(f,combine,zero)(a+1,b))
+
