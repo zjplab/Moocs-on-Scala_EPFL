383c4896dcb043d26c0284148bbeb71375c6f49a
JP Zhang <zjplab@users.noreply.github.com>
2018-03-11 09:44:47 +0800

    Update Finding Fix Points.scala

--- Functional Program Principle in Scala/Higher-Order Functions/Finding Fix Points.scala
+++ Functional Program Principle in Scala/Higher-Order Functions/Finding Fix Points.scala
@@ -1,1 +1,23 @@
+import math.abs 
+
+object exercise{
+  val tolerance=0.0001
+  def isCloseEnough(x:Double,y:Double)=
+    abs((x-y)/x)/x < tolerance 
+  def fixedPoint(f:Double=>Double)(firstGuess:Double)={
+    def iterate（guess: Double):Double={
+      val next=f(guess)
+      if(isCloseEnough(guess,next)) next
+      else iterate(next)
+    }  
+    iterate(firstGuess)
+  }
+  
+  fixedPoint(x=>1+x/2)(1)
+  def sqrt(x:Double)=fixedPoint(y=>x/y)(1)
+  sqrt(2)
+  
+  def averageDamp(f:Double=>Double)(x:Double)=(x+f(x))/2
+  def sqrt(x:Double)=fixedPoint(averageDamp(y=>x/y)(1))
+}
 
